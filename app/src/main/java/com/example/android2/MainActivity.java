f0cf5688a303548b08bf396856d1dac073b9ce42
吳俊緯 <wcw850710@gmail.com>
2022-04-17 11:21:13 +0800

    feat(MainActivity): 完成

--- app/src/main/java/com/example/android2/MainActivity.java
+++ app/src/main/java/com/example/android2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View startersCardView = findViewById(R.id.card_view_starters);
+        startersCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
