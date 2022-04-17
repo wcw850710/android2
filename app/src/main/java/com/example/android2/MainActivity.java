569abe5ebcc52e624adcb6a1739470613260da6e
吳俊緯 <wcw850710@gmail.com>
2022-04-17 13:25:20 +0800

    finished

--- app/src/main/java/com/example/android2/MainActivity.java
+++ app/src/main/java/com/example/android2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -18,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
                 startActivity(startersActivityIntent);
+            }
+        });
+
+        View mailAddress = findViewById(R.id.text_view_mail_address);
+        mailAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:thehungrydeveloper@developer.com")); // only email apps should handle this
+                startActivity(intent);
             }
         });
     }
